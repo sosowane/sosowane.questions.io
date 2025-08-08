2c231e9bc5dfc3045f48b86e9d1850864118fc9d
Sokhna <sosowane01@gmail.com>
2025-08-08 19:22:45 -0400

    Reorganize files for GitHub Pages

--- dynam.js
+++ dynam.js
@@ -1,28 +1,38 @@
 window.onload = function(){ 
-// Get the modal
-var modal = document.getElementById("myModal");
+    // Main Menu Modal
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+    // Suggestion Modal
+    var suggestionModal = document.getElementById("suggestionModal");
+    var suggestionBtn = document.getElementById("suggestBtn");
+    var closesuggestion = document.getElementsByClassName("close-suggestion")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+    suggestionBtn.onclick = function(e) {
+        e.preventDefault();
+        suggestionModal.style.display = "block";
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    closesuggestion.onclick = function() {
+        suggestionModal.style.display = "none";
+    }
 
+    // Close modals when clicking outside
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+        if (event.target == suggestionModal) {
+            suggestionModal.style.display = "none";
+        }
+    }
 };
